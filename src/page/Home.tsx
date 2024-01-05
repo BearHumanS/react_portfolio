1142dca5668fbae084a44eaefc12075ba1d37b0d
BearHumanS <bye0651@naver.com>
2024-01-05 22:57:13 +0900

    Test: 방문자 수 counting 테스트

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,13 @@
+import VisitorCounter from '@components/visitorCounter/VisitorCounter'
 import React from 'react'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      HomePage
+      <VisitorCounter />
+    </div>
+  )
 }
 
 export default HomePage
