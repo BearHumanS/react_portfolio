88b0c50120c9f3754c1ae810ab9a66351d4e91e0
BearHumanS <bye0651@naver.com>
2024-01-07 22:05:05 +0900

    Feat: 웹사이트 방문자 수 단순 카운팅 컴포넌트

--- src/components/visitorCounter/VisitorCounter.tsx
+++ src/components/visitorCounter/VisitorCounter.tsx
@@ -1,14 +1,12 @@
 import { getCount } from '@/lib/api/counterAPI'
 import { useEffect, useState } from 'react'
 import { Cursor, Typewriter } from 'react-simple-typewriter'
-import { motion } from 'framer-motion'
 import { css } from '@emotion/react'
 import Text from '../common/Text'
 import Flex from '../common/Flex'
 
 const VisitorCounter = () => {
   const [count, setCount] = useState(0)
-  const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,46 +20,21 @@ const VisitorCounter = () => {
   }, [])
 
   return (
-    <Flex align="center">
-      {isVisible && (
-        <motion.h1
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
-          transition={{ duration: 2 }}
-          onAnimationComplete={() => setIsVisible(false)}
-        >
-          <Text typography="t1">H</Text>
-        </motion.h1>
-      )}
-      <motion.div
-        initial={{ display: 'none', opacity: 0 }}
-        animate={{ display: 'inline', opacity: 1 }}
-        transition={{ delay: 1, duration: 1 }}
-        css={textStyles}
-      >
+    <Flex align="center" justify="center" css={flexStyles}>
+      <Text typography="t3">
         <Typewriter
-          words={['방문해주셔서 감사합니다', `${count}번 째 방문자님`]}
+          words={['', `방문자 수:   ${count}`]}
           delaySpeed={1000}
           typeSpeed={200}
         />
-
         <Cursor />
-      </motion.div>
-      <motion.h1
-        initial={{ rotateX: 180 }}
-        animate={{ rotateX: 0 }}
-        transition={{ duration: 1 }}
-        className="text-5xl font-bold md:text-9xl"
-      >
-        <Text typography="t1">!</Text>
-      </motion.h1>
+      </Text>
     </Flex>
   )
 }
 
-const textStyles = css`
-  font-size: 32px;
-  line-height: 1.35;
+const flexStyles = css`
+  bottom: 0;
 `
 
 export default VisitorCounter
