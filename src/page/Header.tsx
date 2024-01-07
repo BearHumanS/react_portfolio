13380a2a4dbe608d064fc9fdab5d4c0e577e590f
BearHumanS <bye0651@naver.com>
2024-01-07 22:06:29 +0900

    Feat: Header 컴포넌트 생성 및 navbar 반응형 작업

---
+++ src/page/Header.tsx
@@ -0,0 +1,202 @@
+import Flex from '@/components/common/Flex'
+import { MenuButton } from '@common/navbar/MenuButton'
+import Text from '@/components/common/Text'
+import { breakpoints } from '@/styles/breakPoint'
+import { colors } from '@/styles/color'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import { useEffect, useState, useRef } from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+import Nav from '@/components/common/navbar/Nav'
+import { motion } from 'framer-motion'
+import ScrollIndicator from '@/components/common/ScrollIndicator'
+
+const Header = () => {
+  const [isSticky, setIsSticky] = useState(false)
+  const [menuBtn, setMenuBtn] = useState(false)
+  const [animationComplete, setAnimationComplete] = useState(false)
+
+  const containerRef = useRef(null)
+
+  const handleSmallScreenNav = () => {
+    setMenuBtn(!menuBtn)
+  }
+
+  const handleAnimationComplete = () => {
+    setAnimationComplete(!animationComplete)
+  }
+
+  const checkScroll = () => {
+    const offset = window.scrollY
+    if (offset > 0) {
+      setIsSticky(true)
+    } else {
+      setIsSticky(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScroll)
+    return () => {
+      window.removeEventListener('scroll', checkScroll)
+    }
+  }, [])
+
+  return (
+    <StyleHeader isSticky={isSticky}>
+      <Flex justify="space-between" css={navStyles}>
+        <a href="/">
+          <Text typography="t1" bold>
+            <Typewriter
+              words={['', '남 기 훈']}
+              delaySpeed={1000}
+              typeSpeed={200}
+            />
+          </Text>
+        </a>
+
+        <Flex css={menutStyles}>
+          <ScrollIndicator targetId="Home">
+            <Text typography="t4">
+              <Typewriter
+                words={['', 'Home']}
+                delaySpeed={1000}
+                typeSpeed={200}
+              />
+            </Text>
+          </ScrollIndicator>
+
+          <ScrollIndicator targetId="About">
+            <Text typography="t4">
+              <Typewriter
+                words={['', 'About']}
+                delaySpeed={1000}
+                typeSpeed={200}
+              />
+            </Text>
+          </ScrollIndicator>
+
+          <ScrollIndicator targetId="Project">
+            <Text typography="t4">
+              <Typewriter
+                words={['', 'Project']}
+                delaySpeed={1000}
+                typeSpeed={200}
+              />
+            </Text>
+          </ScrollIndicator>
+
+          <ScrollIndicator targetId="Contact">
+            <Text typography="t4">
+              <Typewriter
+                words={['', 'Contact']}
+                delaySpeed={1000}
+                typeSpeed={200}
+              />
+            </Text>
+          </ScrollIndicator>
+        </Flex>
+        <StylesHamburgerMenu>
+          <MenuButton isOpen={menuBtn} onClick={handleSmallScreenNav} />
+        </StylesHamburgerMenu>
+        <StyleMenuNavigation
+          menuBtn={menuBtn}
+          animationComplete={animationComplete}
+        >
+          <motion.nav
+            initial={false}
+            animate={menuBtn ? 'open' : 'closed'}
+            onAnimationComplete={handleAnimationComplete}
+            ref={containerRef}
+          >
+            <Nav onClick={handleSmallScreenNav} />
+          </motion.nav>
+        </StyleMenuNavigation>
+      </Flex>
+    </StyleHeader>
+  )
+}
+
+const StyleHeader = styled.header(({ isSticky }: { isSticky: boolean }) => ({
+  width: '100%',
+  height: 70,
+  backdropFilter: 'blur(10px)',
+  zIndex: 50,
+  position: 'sticky',
+  top: 0,
+  transition: 'easeInOut 0.3s',
+  borderBottom: isSticky ? `1px solid ${colors.grey2}` : 'none',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+
+  [`@media (min-width: ${breakpoints.sm})`]: {
+    maxWidth: '640px',
+  },
+  [`@media (min-width: ${breakpoints.md})`]: {
+    maxWidth: '768px',
+  },
+  [`@media (min-width: ${breakpoints.lg})`]: {
+    maxWidth: '1024px',
+  },
+  [`@media (min-width: ${breakpoints.xl})`]: {
+    maxWidth: '1280px',
+  },
+  [`@media (min-width: ${breakpoints['2xl']})`]: {
+    maxWidth: '1536px',
+  },
+}))
+
+const navStyles = css`
+  width: 100%;
+`
+
+const menutStyles = css`
+  gap: 14px;
+  display: none;
+
+  @media (min-width: ${breakpoints.md}) {
+    display: flex;
+  }
+`
+
+const StylesHamburgerMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (min-width: ${breakpoints.md}) {
+    display: none;
+  }
+`
+
+const StyleMenuNavigation = styled.div(
+  ({
+    menuBtn,
+    animationComplete,
+  }: {
+    menuBtn: boolean
+    animationComplete: boolean
+  }) => ({
+    position: 'absolute',
+    top: '100px',
+    right: 0,
+    bottom: 0,
+    left: menuBtn || animationComplete ? '0' : '-150%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    width: '100%',
+    height: '100vh',
+    backgroundColor: 'slategray',
+    color: 'white',
+    textAlign: 'center',
+    transition: 'ease-in 0.3s',
+    paddingTop: '150px',
+
+    [`@media (min-width: ${breakpoints.md})`]: {
+      display: 'none',
+    },
+  }),
+)
+
+export default Header
