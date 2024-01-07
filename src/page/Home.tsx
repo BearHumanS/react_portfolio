03e66b0868c1958e5a588cc105bbc16fa457a76e
BearHumanS <bye0651@naver.com>
2024-01-07 22:06:52 +0900

    Feat: home page 생성

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,13 +1,40 @@
+import { breakpoints } from '@styles/breakPoint'
 import VisitorCounter from '@components/visitorCounter/VisitorCounter'
-import React from 'react'
+import styled from '@emotion/styled'
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <Container id="Home">
+      <div>HomePage</div>
       <VisitorCounter />
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+
+  @media (min-width: ${breakpoints.sm}) {
+    max-width: 640px;
+  }
+  @media (min-width: ${breakpoints.md}) {
+    max-width: 768px;
+  }
+  @media (min-width: ${breakpoints.lg}) {
+    max-width: 1024px;
+  }
+  @media (min-width: ${breakpoints.xl}) {
+    max-width: 1280px;
+  }
+  @media (min-width: ${breakpoints['2xl']}) {
+    max-width: 1536px;
+  }
+`
 
 export default HomePage
