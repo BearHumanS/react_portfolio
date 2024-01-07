516b04e3292bc6299c234c7e1f3697a414d459b9
BearHumanS <bye0651@naver.com>
2024-01-07 22:07:02 +0900

    Feat: 포트폴리오 메인페이지 생성

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,13 +1,37 @@
+import { breakpoints } from '@/styles/breakPoint'
+import styled from '@emotion/styled'
+import About from './About'
+import ContactPage from './Contact'
+import Footer from './Footer'
+import Header from './Header'
 import HomePage from './Home'
 import Project from './Project'
 
 const MainPage = () => {
   return (
-    <div>
+    <MainContainer>
+      <Header />
       <HomePage />
+      <About />
       <Project />
-    </div>
+      <ContactPage />
+      <Footer />
+    </MainContainer>
   )
 }
+
+const MainContainer = styled.main`
+  position: relative;
+  width: 92%;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: ${breakpoints.md}) {
+    width: 80%;
+  }
+`
 
 export default MainPage
