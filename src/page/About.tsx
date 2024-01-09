41f14c5ccec778334e9cae2d2172debb13b17d01
BearHumanS <bye0651@naver.com>
2024-01-10 04:20:43 +0900

    Feat: about 페이지 레이아웃 구성

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,5 +1,19 @@
+import About from '@/components/about/About'
+import styled from '@emotion/styled'
+
 const AboutPage = () => {
-  return <div id="About">About</div>
+  return (
+    <AboutContainer id="About">
+      <About />
+    </AboutContainer>
+  )
 }
+
+const AboutContainer = styled.section`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  margin-top: 70px;
+`
 
 export default AboutPage
