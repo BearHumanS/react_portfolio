6720271ecec2ae47b220ffb1b5d337273078c289
BearHumanS <bye0651@naver.com>
2024-01-10 04:21:27 +0900

    Feat: 색상 추가

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -4,22 +4,38 @@ export const colorTheme = css`
   :root {
     --red: #f44336;
     --blue: #459ee5;
-    --green: #4caf50;
+    --green1: #4caf50;
+    --green2: #1098ad;
+    --green3: #07b39b;
+    --green4: #6fba82;
     --white: #fff;
-    --black: #212121;
+    --black: #000;
     --grey: #eee;
     --grey2: #a6b5c1;
+    --yellow: #f79533;
+    --pink1: #f37055;
+    --pink2: #ef4e7b;
+    --puple: #a166ab;
+    --puple2: #5073b8;
   }
 `
 
 export const colors = {
   red: 'var(--red)',
   blue: 'var(--blue)',
-  green: 'var(--green)',
+  green1: 'var(--green1)',
+  green2: 'var(--green2)',
+  green3: 'var(--green3)',
+  green4: 'var(--green4)',
   white: 'var(--white)',
   black: 'var(--black)',
   grey: 'var(--grey)',
   grey2: 'var(--grey2)',
+  yellow: 'var(--yellow)',
+  pink1: 'var(--pink1)',
+  pink2: 'var(--pink2)',
+  puple: 'var(--puple)',
+  puple2: 'var(--puple2)',
 }
 
 export type Colors = keyof typeof colors
