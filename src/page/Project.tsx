cc60020f8fab02551bab1b9b00d3c30eb421757a
BearHumanS <bye0651@naver.com>
2024-01-23 02:25:18 +0900

    Feat: 프로젝트 상세 페이지 라우터 관련 작업 및 AnimatePresence를 사용하여 프로젝트 상세 정보의 등장과 사라짐에 애니메이션 적용

--- src/page/Project.tsx
+++ src/page/Project.tsx
@@ -1,11 +1,18 @@
-import Project from '@/components/project/Project'
+import List from '@/components/project/List'
+import Project from '@/components/project/ProjectTest'
 import styled from '@emotion/styled'
+import { AnimatePresence } from 'framer-motion'
+import { useParams } from 'react-router-dom'
 
 const ProjectPage = () => {
+  const { id } = useParams()
   return (
-    <ProjectContainer id="Project">
-      <Project />
-    </ProjectContainer>
+    <>
+      <ProjectContainer id="Project">
+        <AnimatePresence>{id && <Project params={id} />}</AnimatePresence>
+      </ProjectContainer>
+      <List selectedId={id !== undefined ? id : ''} />
+    </>
   )
 }
 
