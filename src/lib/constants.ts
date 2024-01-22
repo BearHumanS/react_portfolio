0f87cbacf7387bc2b88d77d9da980ecf16a6064b
BearHumanS <bye0651@naver.com>
2024-01-23 04:06:56 +0900

    Refactor: key value형태로 영문 한글로 변경

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,6 @@
-export const ITEMS = ['Home', 'About', 'Project', 'Contact']
+export const ITEMS = [
+  { en: 'Home', ko: '홈' },
+  { en: 'About', ko: '소개' },
+  { en: 'Project', ko: '프로젝트' },
+  { en: 'Contact', ko: '연락처' },
+]
